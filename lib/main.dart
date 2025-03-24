36560ecdb966da65e3fd6331f653193c1ad1c6d9
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-03-24 16:07:23 -0300

    folder realocation

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consumo de dados metereológicos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
